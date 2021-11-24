f4b119f765c78f36cb5f409b666c468578cdaf90
sirolf-otrebla <albeflor@gmail.com>
2021-11-24 10:45:51 +0100

    fix errors

--- examples/deep_svdd/loader.py
+++ examples/deep_svdd/loader.py
@@ -76,7 +76,7 @@ cfg_pool = [
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Deep SVDD network for CompositeNet. You can load an external configuration or use the example one already in the code')
-    parser.add_argument('configs', metavar='C', nargs='+', required=False, default=None,
+    parser.add_argument('configs', metavar='C', nargs='*', required=False, default=None,
                         help='json path to desired network configuration. you can add more than one configuration and run them one after the other')
     args = parser.parse_args()
     if args.configs != None:
